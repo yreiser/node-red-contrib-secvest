fe45d5e685a58816e4e42c5e0cb6d0dbf84f3f17
Yves Reiser <develop@yvesreiser.lu>
2022-09-17 23:48:51 +0200

    deleted all

--- status.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (RED) {
-    const fs = require('fs');
-    const path = require("path");
-    const file = path.join(__dirname, "status.json");
-    const text = fs.readFileSync(file);
-    const flow = JSON.parse(text);
-    RED.nodes.registerSubflow(flow);
-}
